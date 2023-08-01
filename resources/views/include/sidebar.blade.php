b3b4031299a28bb8eda6cfca351b35e732e21066
ielemson <ielemson@gmail.com>
2023-08-01 01:01:45 +0100

    updated icon

--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -50,7 +50,7 @@
              @endhasrole
 {{-- CUSTOM SIDEBAR  :::::::::::::::::::::::::::::: STARTS--}}
 <div class="nav-item {{ ($segment1 == 'users' || $segment1 == 'roles'||$segment1 == 'permission' ||$segment1 == 'user') ? 'active open' : '' }} has-sub">
-    <a href="#"><i class="ik ik-user"></i><span>{{ __('Services')}}</span></a>
+    <a href="#"><i class="ik ik-settings"></i><span>{{ __('Services')}}</span></a>
     <div class="submenu-content">
         <!-- only those have manage_user permission will get access -->
         <a href="{{url('service/create')}}" class="menu-item {{ ($segment1 == 'user' && $segment2 == 'create') ? 'active' : '' }}">{{ __('Add Service')}}</a>
@@ -61,7 +61,7 @@
 
 {{-- News Route Starts Here::::::::::::::::::::::::::: --}}
 <div class="nav-item {{ ($segment1 == 'news' || $segment1 == 'roles'||$segment1 == 'permission' ||$segment1 == 'user') ? 'active open' : '' }} has-sub">
-    <a href="#"><i class="ik ik-user"></i><span>{{ __('News')}}</span></a>
+    <a href="#"><i class="ik ik-file-text"></i><span>{{ __('News')}}</span></a>
     <div class="submenu-content">
         <!-- only those have manage_user permission will get access -->
         <a href="{{route('news.create')}}" class="menu-item {{ ($segment1 == 'news' && $segment2 == 'create') ? 'active' : '' }}">{{ __('Create News')}}</a>
@@ -75,7 +75,7 @@
 {{-- News Route Ends Here::::::::::::::::::::::::::: --}}
 {{-- Features --}}
 <div class="nav-item {{ ($segment1 == 'features' || $segment1 == 'roles'||$segment1 == 'permission' ||$segment1 == 'user') ? 'active open' : '' }} has-sub">
-    <a href="#"><i class="ik ik-user"></i><span>{{ __('Features')}}</span></a>
+    <a href="#"><i class="ik ik-star"></i><span>{{ __('Features')}}</span></a>
     <div class="submenu-content">
         <!-- only those have manage_user permission will get access -->
         <a href="{{url('feature/list')}}" class="menu-item {{ ($segment1 == 'user' && $segment2 == 'create') ? 'active' : '' }}">{{ __('Features')}}</a>
