e8d64a54ab79913b32f4aeeacf4134d730d0fa3f
ielemson <ielemson@gmail.com>
2023-08-12 07:31:56 +0100

    updated contact us and homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -100,10 +100,11 @@ class HomeController extends Controller
         $services = Service::all();
         $categories = NewsCategory::where('status',1)->get();
         $getNewsCategory = NewsCategory::where('slug',$slug)->first();
+        $topnewslist = News::latest()->whereHas('category')->where('status',1)->orderBy('id', 'DESC')->paginate(10);
         // dd($getNewsCategory);
         $news = News::where('category_id',$getNewsCategory->id)->paginate(5);
         // dd($news);
-        return view('frontend.pages.news',compact('services','news','categories'));
+        return view('frontend.pages.news',compact('services','news','categories','topnewslist'));
     }
 
     public function StoreUser(Request $request){
